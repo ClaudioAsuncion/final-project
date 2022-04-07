8d09afb6121754acb651c0841723a61a7dc71a8e
flgirl813 <59990889+flgirl813@users.noreply.github.com>
2022-04-06 23:42:52 -0400

    updated js

--- js/index.js
+++ js/index.js
@@ -6,3 +6,9 @@ newTask2.addTask("JS", "Test", "Tierra", 10, status);
 // console.log(newTask2);
 
 // Event Listener for task form
+
+document.getElementById("submitBtn").addEventListener("click", submit);
+
+function submit() {
+  alert("its working");
+}
